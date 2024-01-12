1d193180979d8fba633b2bd594c05acdc70cbe31
criggins44 <128842735+criggins44@users.noreply.github.com>
2024-01-12 15:57:28 -0500

    created a setData method to store items into local storage

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-script
+let addTodo = () => {
+	let todoText = document.getElementById(todo-text).value;
+	if (todoText != ''){
+		setData(todoText); //adding item to local storage
+		listTodo(); //showing item from local storage
+	}
+}
+
+let setData = (item) => {
+	if (getData(item) != false) {
+		alert ("item already added in todo")
+	} else {
+		let data = getData(); //call getData handler for getting data from list
+		data = (data != false) ? data : [];
+		data.push(item);
+		data = JSON.stringify(data);
+		localStorage.setItem('mytodo', data);
+	}
+}
