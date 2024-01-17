96954b30177d1e736b5e6f9ae18754f1fa0290a1
criggins44 <128842735+criggins44@users.noreply.github.com>
2024-01-17 12:37:55 -0500

    added todo function to add tasks to local storage

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+var items = JSON.parse(localStorage.getItem("todo-list")) || [];
 
+function addTodo(){
+	let inputBox = document.querySelector(#todo-input);
+	let item = inputBox.value;
 
+	if (item ==="")
+		return (document.getElementById("list").innerHTML = "Insert a task");
+
+	items.push({
+		value: item,
+	});
+
+	localStorage.setItem("todo-list", JSON.stringify(items));
+	listItems();
+	inputBox.value ="";
+}
